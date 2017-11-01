e6076a708192e818ad0bc26d2bd15f8281eac4bd
Gavin Kendall <gkendall@igloosoftware.com>
2017-11-01 12:47:59 -0400

    Added the "ScreenPropertyTest" project.

---
+++ ScreenPropertyTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace ScreenPropertyTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*** Primary Screen (Bounds) ***");
+            Console.WriteLine("Width: " + Screen.PrimaryScreen.Bounds.Width);
+            Console.WriteLine("Height: " + Screen.PrimaryScreen.Bounds.Height);
+
+            Console.WriteLine("\n*** Primary Screen (Working Area) ***");
+            Console.WriteLine("Width: " + Screen.PrimaryScreen.WorkingArea.Width);
+            Console.WriteLine("Height: " + Screen.PrimaryScreen.WorkingArea.Height);
+
+            Console.WriteLine("\n*** Virtual Screen (SysParam) ***");
+            Console.WriteLine("Width: " + SystemParameters.VirtualScreenWidth);
+            Console.WriteLine("Height: " + SystemParameters.VirtualScreenHeight);
+
+            Console.WriteLine("\n*** Virtual Screen (SysInfo) ***");
+            Console.WriteLine("Width: " + SystemInformation.VirtualScreen.Width);
+            Console.WriteLine("Height: " + SystemInformation.VirtualScreen.Height);
+
+            Console.ReadLine();
+        }
+    }
+}
