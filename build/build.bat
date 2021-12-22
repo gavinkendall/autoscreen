04372c5065993741575aeee017ed76cbe3f0863c
Gavin Kendall <gavinkendall@gmail.com>
2021-12-22 14:08:32 -0500

    2.4.0.0 Build and release is now more streamlined.

--- build/build.bat
+++ build/build.bat
@@ -1,7 +1,12 @@
+rmdir /s /q !autoscreen
+rmdir /s /q screenshots
+del /s /q autoscreen.conf
+del /s /q autoscreen.exe
+del /s /q AutoScreenCaptureSetup.msi
 del /s /q ..\bin\Release\*.*
 rmdir /s /q ..\bin\Release\!autoscreen
 rmdir /s /q ..\bin\Release\screenshots
-msbuild ..\autoscreen_vs2019.sln /p:Configuration=Release -restore
+devenv ..\autoscreen_vs2019.sln /Project ..\AutoScreenCapture.csproj /Build Release
 signtool sign /f ..\autoscreen.pfx /p Sonic2020! /fd SHA256 ..\bin\Release\AutoScreenCapture.exe
 signtool sign /f ..\autoscreen.pfx /p Sonic2020! /fd SHA256 ..\bin\Release\Renci.SshNet.dll
 signtool sign /f ..\autoscreen.pfx /p Sonic2020! /fd SHA256 ..\bin\Release\Gavin.Kendall.SFTP.dll
@@ -13,4 +18,9 @@ del ..\bin\Release\Gavin.Kendall.SFTP.dll
 del ..\bin\Release\Gavin.Kendall.SFTP.pdb
 del ..\bin\Release\Renci.SshNet.dll
 del ..\bin\Release\Renci.SshNet.xml
-signtool sign /f ..\autoscreen.pfx /p Sonic2020! /fd SHA256 ..\bin\Release\autoscreen.exe
+devenv ..\autoscreen_vs2019.sln /Project ..\AutoScreenCaptureSetup\AutoScreenCaptureSetup.vdproj /Build Release
+move ..\bin\Release\autoscreen.exe .
+move ..\AutoScreenCaptureSetup\Release\AutoScreenCaptureSetup.msi .
+del ..\AutoScreenCaptureSetup\Release\setup.exe
+signtool sign /f ..\autoscreen.pfx /p Sonic2020! /fd SHA256 autoscreen.exe
+signtool sign /f ..\autoscreen.pfx /p Sonic2020! /fd SHA256 AutoScreenCaptureSetup.msi
